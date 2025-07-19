2cc75b9a7055aebf1513ba6f1e4d517edd078591
dema89 <42239642+dema89@users.noreply.github.com>
2025-07-19 18:01:46 +0200

    Add files via upload

--- plugins/loadjson.js
+++ plugins/loadjson.js
@@ -1,22 +1,5 @@
-export default defineNuxtPlugin(async (nuxtApp) => {
-  try {
-    let data
+import data from '~/public/data/data.json'
 
-    if (process.server) {
-      // Import diretto del file statico
-      data = await import('~/public/data/data.json').then(m => m.default)
-    } else {
-      // In client possiamo usare fetch normalmente
-      const response = await fetch('/data/data.json')
-      if (!response.ok) {
-        console.error('Failed to fetch data:', response.statusText)
-        return
-      }
-      data = await response.json()
-    }
-
-    nuxtApp.provide('movies', data)
-  } catch (error) {
-    console.error('Errore durante il caricamento dei dati:', error)
-  }
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.provide('movies', data)
 })
