72bd6d4777c1de380d4a171bc395aefad1b945e2
dema89 <42239642+dema89@users.noreply.github.com>
2025-07-19 20:06:50 +0200

    Add files via upload

--- plugins/loadjson.js
+++ plugins/loadjson.js
@@ -1,18 +1,5 @@
-export default defineNuxtPlugin(async (nuxtApp) => {
-  try {
-    // Percorso assoluto basato su baseURL di Nuxt (es. '/prova/')
-    const config = useRuntimeConfig()
-    const base = config.app.baseURL || '/'
-    const response = await fetch(`${base}data/data.json`)
+import data from '~/public/data/data.json'
 
-    if (!response.ok) {
-      console.error('Failed to fetch data:', response.statusText)
-      return
-    }
-
-    const data = await response.json()
-    nuxtApp.provide('movies', data)
-  } catch (error) {
-    console.error('Errore durante il caricamento dei dati:', error)
-  }
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.provide('movies', data)
 })
