63e39a5e7d58e8794094b5f8be1f15c67a1b2760
dema89 <42239642+dema89@users.noreply.github.com>
2025-07-28 21:46:23 +0200

    Add files via upload

--- pages/film/[nome].vue
+++ pages/film/[nome].vue
@@ -25,8 +25,8 @@
             </svg>
           </a>
 
-          <img id="cover_img" fetchpriority="high" :src="`/img/${movieDetails.cover}`" ref="coverImgRef" />
-          <img id="cover_img_blend" fetchpriority="high" :src="`/img/${movieDetails.cover2}`" ref="coverImgBlendRef" />
+          <img id="cover_img" fetchpriority="high" v-if="movieDetails?.cover" :src="`/img/${movieDetails.cover}`" ref="coverImgRef" />
+          <img id="cover_img_blend" fetchpriority="high" v-if="movieDetails?.cover"  :src="`/img/${movieDetails.cover2}`" ref="coverImgBlendRef" />
         </div>
       </div>
     </section>
@@ -62,8 +62,8 @@
           :data-color-good="movieDetails.datacolor"
           :data-color-evil="movieDetails.datacolor2"
         >
-          <img :alt="movieDetails.titolo" loading="lazy" :src="`/img/${movieDetails.poster}`" />
-          <img :alt="movieDetails.titolo" loading="lazy" :src="`/img/${movieDetails.poster2}`" />
+          <img :alt="movieDetails.titolo" loading="lazy" v-if="movieDetails?.poster" :src="`/img/${movieDetails.poster}`" />
+          <img :alt="movieDetails.titolo" loading="lazy" v-if="movieDetails?.poster2" :src="`/img/${movieDetails.poster2}`" />
         </div>
         <div id="light-spot" ref="lightSpot"></div>
       </div>
