84333390cefa312b033af9ae2da5378d21d58be4
dema89 <42239642+dema89@users.noreply.github.com>
2025-07-28 22:10:00 +0200

    Add files via upload

---
+++ pages/[...slug].vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>Page not found: {{ $route.fullPath }}</p>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, nextTick, watch, watchEffect, onUnmounted, inject } from 'vue'
+import { useRoute } from '#app'
+
+const { $gsap, $movies, $splitTextAnimation, $splitTextAnimationImmediate, $lenis } = useNuxtApp()
+const { params } = useRoute()
+
+definePageMeta({
+  layout: false // se vuoi una pagina senza layout
+})
+</script>
