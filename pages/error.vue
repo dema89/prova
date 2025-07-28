aed632e5492e03b9405c734c132d84005aec0773
dema89 <42239642+dema89@users.noreply.github.com>
2025-07-28 22:06:12 +0200

    Add files via upload

--- pages/error.vue
+++ pages/error.vue
@@ -1,3 +1,13 @@
 <template>
-  404
+  <div class="error-page">
+    <h1>{{ error.statusCode }}</h1>
+    <p>{{ error.message }}</p>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+</script>
