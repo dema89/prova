7f15ea2e9d5f9f57aa16587966c730e5c3b000ea
dema89 <42239642+dema89@users.noreply.github.com>
2025-09-26 12:34:43 +0200

    Add files via upload

---
+++ pages/privacy.vue
@@ -0,0 +1,43 @@
+<template>
+  <section id="privacy">
+    <div>
+      <h1>Privacy Policy</h1>
+
+      <h2>Data Controller</h2>
+      <p>
+      Studio K95 is a brand of Arti Grafiche Leonardi S.R.L.
+      <br>Via Vittorio Emanuele II 140, 95131 Catania, Italy
+      <br>Email: webservice@k95.it
+      </p>
+
+      <h2>Data Collected</h2>
+
+      <p>This website does not collect or process any personal data from its users.
+    There are no contact forms, registration systems, or other means of collecting personal information.
+      </p>
+
+      <h2>Cookie</h2>
+
+      <p>This website only uses technical cookies strictly necessary for its proper functioning (for example, to enable navigation or display of content).
+    No profiling cookies, marketing tools, or third-party tracking services are used.</p>
+
+      <h2>Purpose of Processing</h2>
+      <p>The only processing carried out concerns technical cookies, whose sole purpose is to ensure the correct operation of the website.
+    No analytics, profiling, or collection of personal information is performed.</p>
+
+      <h2>Legal Basis of Processing</h2>
+      <p>The use of technical cookies is based on Article 6(1)(f) of the GDPR, as it is necessary for the legitimate interest of the Data Controller to ensure the proper functioning of the website.</p>
+
+      
+      <h2>User Rights</h2>
+
+      <p>In accordance with Regulation (EU) 2016/679 (GDPR), users may contact the Data Controller for any clarification regarding the possible processing of their personal data.
+    Since this website does not collect any personal data, these rights are not practically applicable, but users may still contact the Data Controller using the details provided above.</p>
+
+      <h2>Changes to This Policy</h2>
+
+      <P>The Data Controller reserves the right to make changes to this Privacy Policy at any time. Any updates will be published on this page.</P>
+    </div>
+  </section>
+</template>
+
